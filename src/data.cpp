09b20c5fa372e4aef5e3f2e4c5181ca53d7d9427
Ivan Konstantinov <vankata1998@users.noreply.github.com>
2020-06-11 19:37:19 +0300

    comments

--- src/data.cpp
+++ src/data.cpp
@@ -3,56 +3,39 @@
 #include <algorithm>
 struct Data
 {
-    std::vector<double> data;
     //Vector containing all the elements
-    double median;
-    //Variable to keep the value of the median
+    std::vector<double> data;
 
+    //Variable to keep the value of the median
+    double median;
+
+    //Function that adds new element to the existing vector
     void add(int a)
     {
         data.push_back(a);
     }
-    //Function that adds new element to the existing vector
 
+    //Function to print the existing vector
     void printData()
     {
         for (std::vector<double>::iterator it = data.begin(); it < data.end(); it++)
-            std::cout << ' ' << *it;
+            std::cout << " "  << *it;
     }
-    //Function to print the existing vector
 
-    void sort()
-    {
-        //int temp;
-        std::sort(data.begin(), data.end());
-        /*	for (int i = 0; i < data.size(); i++)
-            {
-                for (int j = i + 1; j < data.size(); j++)
-                {
-                    if (data[j] < data[i])
-                    {
-                        temp = data[i];
-                        data[i] = data[j];
-                        data[j] = temp;
-                    }
-                }
-            }*/
-    }
-    //Sort Function
-
+    //Function to get the median of the existing vector after it has been sorted
     void getMedian()
     {
+        std::sort(data.begin(), data.end());
         if (data.size() % 2 == 0)
         {
             median = (data[data.size() / 2] + data[data.size() / 2 - 1]) / 2;
-            std::cout << " " << median;
+            std::cout << std::endl << "median : " << median;
         }
         else
         {
             median = data[data.size() / 2];
-            std::cout << " " << median;
+            std::cout << std::endl << "median : "  << median;
         }
     }
-    //Function to get the median of the existing vector after it has been sorted
 
 };
